9d3ac47c453a5c6b773d8cd1b1e057a161417860
abel1502 <terragame.abel@gmail.com>
2020-09-09 16:11:45 +0300

    Shakespeare add docs

--- Shakespeare/main.cpp
+++ Shakespeare/main.cpp
@@ -22,22 +22,94 @@ const int MAX_LINES = 0x4000;
 
 //--------------------------------------------------------------------------------
 
+// TODO: Document exceptional return values
+
+/**
+ * A type for a russian-compatible letter
+ */
 typedef unsigned char letter;
 
-typedef struct lines {  // Gonna be a heap
+/**
+ * An array of lines of a poem
+ */
+typedef struct lines {
     int len;
     letter ** vals;
 } lines_t;
 
+/**
+ * Shows a constant banner at the beginning of execution
+ */
 void showBanner(void);
+
+/**
+ * Shows help on how to use the program
+ */
 void showUsage(const char * binname);
+
+/**
+ * Reads a line from a file
+ *
+ * @param [in]  ifile   The input file
+ * @param [out] line    The destination
+ * @param [in]  maxLen  A limit to how long the line may be
+ */
 int readLine(FILE * ifile, letter * line, int maxLen);
+
+/**
+ * Reads all lines from a file
+ *
+ * @param [in]  ifile     The input file
+ * @param [out] lines     The destination
+ * @param [in]  maxLines  A limit to how many lines there may be
+ */
 int readLines(FILE * ifile, lines_t * lines, int maxLines);
+
+/**
+ * Writes all lines to a file
+ *
+ * @param [in]  ofile  The output file
+ * @param [in]  lines  The lines to write
+ */
 int writeLines(FILE * ofile, lines_t * lines);
+
+/**
+ * A constructor for lines
+ *
+ * @param [out] lines  The lines to be initialized
+ */
 int initLines(lines_t * lines);
-void sortLines(lines_t * lines);
+
+/**
+ * Quick sorts lines based on a comparator
+ *
+ * @param [in,out] lines  The lines to be sorted
+ * @param [in]     cmp    The comparator function.
+ *   Real signature: int cmp(const letter ** a, const letter ** b).
+ *   Should return the equivalent of a - b
+ */
+void sortLines(lines_t * lines, int (*cmp)(const void *, const void *));
+
+/**
+ * Checks if a letter is an alphabetic character in Latin or Cyrillic
+ *
+ * @param [in]  c  The letter to be checked
+ */
 bool isLetter(letter c);
-int cmpLines(const void * first, const void * second);  // Attention: compares two strings, not lines_t!
+
+/**
+ * A comparator function for lines that compares two lines,
+ *  ignoring the non-alphabetical characters
+ *
+ * @param [in]  a, b  The first lines to be compared. (Actual type: letter**)
+ */
+int cmpLines(const void * a, const void * b);  // Attention: compares two strings, not lines_t!
+
+/**
+ * Frees the memory allocated by initLines
+ *
+ * @param [in,out]  lines  The same lines as what you passed to initLines
+ */
 void freeLines(lines_t * lines);
 
 //================================================================================
@@ -69,7 +141,7 @@ int main(const int argc, const char **argv) {
     }
 
     //printf("%d\n", lines.len);
-    sortLines(&lines);
+    sortLines(&lines, cmpLines);
 
     char newName[32] = "sorted_";
     if (strlen(newName) + strlen(argv[1]) > sizeof(newName)) {
@@ -172,28 +244,28 @@ int initLines(lines_t * lines) {
     return 0;
 }
 
-void sortLines(lines_t * lines) {
-    qsort(lines->vals, lines->len, sizeof(lines->vals[0]), cmpLines);
+void sortLines(lines_t * lines, int (*cmp)(const void *, const void *)) {
+    qsort(lines->vals, lines->len, sizeof(lines->vals[0]), cmp);
 }
 
 bool isLetter(letter c) {
     return isalpha(c) || ((letter)'à' <= c && c <= (letter)'ÿ') || ((letter)'À' <= c && c <= (letter)'ß');
 }
 
-int cmpLines(const void * first, const void * second) {
-    letter * first_s = *(letter **)first;
-    letter * second_s = *(letter **)second;
+int cmpLines(const void * a, const void * b) {
+    letter * a_str = *(letter **)a;
+    letter * b_str = *(letter **)b;
 
     int i = 0, j = 0;
-    while (i < MAX_LINE && j < MAX_LINE && first_s[i] && second_s[j]) {
-        if (isLetter(first_s[i]) && isLetter(second_s[j])) {
-            int res = (int)first_s[i] - (int)second_s[j];
+    while (i < MAX_LINE && j < MAX_LINE && a_str[i] && b_str[j]) {
+        if (isLetter(a_str[i]) && isLetter(b_str[j])) {
+            int res = (int)a_str[i] - (int)b_str[j];
             if (res) return res;
             i++; j++;
             continue;
         }
-        if (!isLetter(first_s [i])) i++;
-        if (!isLetter(second_s[j])) j++;
+        if (!isLetter(a_str[i])) i++;
+        if (!isLetter(b_str[j])) j++;
     }
     return 0;
 }
