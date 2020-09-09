a14eebb71675cd395491b921368a3ce5bbf3b512
abel1502 <terragame.abel@gmail.com>
2020-09-09 12:57:36 +0300

    Shakespeare complete forward sort Onegin

--- Shakespeare/main.cpp
+++ Shakespeare/main.cpp
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <locale.h>
+#include <ctype.h>
 
 
 #if !NDEBUG  // Okay, not gonna bother this time
@@ -16,19 +18,33 @@
 //--------------------------------------------------------------------------------
 
 const int MAX_LINE = 80;
-const int MAX_LINES = 2048;
+const int MAX_LINES = 0x4000;
 
 //--------------------------------------------------------------------------------
 
+typedef unsigned char letter;
+
+typedef struct lines {  // Gonna be a heap
+    int len;
+    letter ** vals;
+} lines_t;
+
 void showBanner(void);
-void showUsage(char * binname);
-long int fsize(FILE * ifile);
-int readLine(FILE * ifile, char * line, int maxLen);
-int readLines(FILE * ifile, char ** lines, int maxLines);
+void showUsage(const char * binname);
+int readLine(FILE * ifile, letter * line, int maxLen);
+int readLines(FILE * ifile, lines_t * lines, int maxLines);
+int writeLines(FILE * ofile, lines_t * lines);
+int initLines(lines_t * lines);
+void sortLines(lines_t * lines);
+bool isLetter(letter c);
+int cmpLines(const void * first, const void * second);  // Attention: compares two strings, not lines_t!
+void freeLines(lines_t * lines);
 
 //================================================================================
 
-int main(int argc, char **argv) {
+int main(const int argc, const char **argv) {
+    setlocale(LC_ALL, "Russian");
+
     TEST_MAIN(
         //
         ,
@@ -43,22 +59,33 @@ int main(int argc, char **argv) {
     }
 
     FILE * ifile = fopen(argv[1], "r");
-    char ** lines = (char **) malloc(sizeof(char *) * MAX_LINES);
-    if (lines == NULL) {
-        ERR("Can't allocate space for lines");
-        return EXIT_FAILURE;
-    }
-    int lineCnt = readLines(ifile, lines, MAX_LINES);
-    if (lineCnt < 0) {
+    lines_t lines;
+    initLines(&lines);
+    int result = readLines(ifile, &lines, MAX_LINES);
+    fclose(ifile);
+    if (result) {
         ERR("Error while reading lines");
         return EXIT_FAILURE;
     }
-    printf("%d\n", lineCnt);
 
-    for (int i = 0; i < lineCnt; ++i) {
-        free(lines[i]);
+    //printf("%d\n", lines.len);
+    sortLines(&lines);
+
+    char newName[32] = "sorted_";
+    if (strlen(newName) + strlen(argv[1]) > sizeof(newName)) {
+        ERR("Binary\'s name is too long");
+        return EXIT_FAILURE;
     }
-    free(lines);
+    strcat(newName, argv[1]);
+    FILE * ofile = fopen(newName, "w");
+    result = writeLines(ofile, &lines);
+    fclose(ofile);
+    if (result) {
+        ERR("Error while writing lines");
+        return EXIT_FAILURE;
+    }
+
+    freeLines(&lines);
     return EXIT_SUCCESS;
 }
 
@@ -72,30 +99,25 @@ void showBanner(void) {
            "#                              #\n"
            "################################\n");
     printf("This program sorts a Shakespearean poem in several funky ways.\n"
-           "I'm too lazy to explain. Just give it a try).\n\n");
+           "I\'m too lazy to explain. Just give it a try).\n"
+           "\nEDIT: Okay, apparently Hamlet isn\'t a real poem, so I hereby\n"
+           "proclaim that this also works on Eugene Onegin\n\n");
 }
 
-void showUsage(char * binname) {
+void showUsage(const char * binname) {
     printf("Usage: %s ifile\n\n"
            "ifile - The file containing the source poem\n"
            "(The results are placed in files with the same names with extra prefixes)\n\n", binname);
 }
 
-
-long int fsize(FILE * ifile) {
-    fseek(ifile, 0, SEEK_END);
-    long int isize = ftell(ifile);
-    fseek(ifile, 0, SEEK_SET);
-    return isize;
-}
-
-
-int readLine(FILE * ifile, char * line, int maxLen) {
+int readLine(FILE * ifile, letter * line, int maxLen) {
     int cur = fgetc(ifile);
-    for (int pos = 0; cur != EOF && cur != '\n' && pos < maxLen; ++pos) {
+    int pos = 0;
+    for (; cur != EOF && cur != '\n' && pos < maxLen; ++pos) {
         line[pos] = cur;
         cur = fgetc(ifile);
     }
+    line[pos] = '\0';
     if (cur == '\n') {
         return 0;
     } else if (cur == EOF) {
@@ -106,20 +128,81 @@ int readLine(FILE * ifile, char * line, int maxLen) {
     }
 }
 
-int readLines(FILE * ifile, char ** lines, int maxLines) {
+int readLines(FILE * ifile, lines_t * lines, int maxLines) {
     int state = 0;
     int i = 0;
     for (; state == 0 && i < maxLines; ++i) {
-        lines[i] = (char *) malloc(sizeof(char) * MAX_LINE);
-        if (lines[i] == NULL) {
-            ERR("Can't allocate space for a line");
-            return -1;
+        lines->vals[i] = (letter *) malloc(sizeof(letter) * MAX_LINE);
+        if (lines->vals[i] == NULL) {
+            ERR("Can\'t allocate space for a line");
+            return 1;
         }
-        state = readLine(ifile, lines[i], MAX_LINE);
+        state = readLine(ifile, lines->vals[i], MAX_LINE - 1);
+        lines->len++;
+    }
+    if (state == 1) { // Empty last line
+        free(lines->vals[lines->len - 1]);
+        lines->len--;
     }
     if (state == 2) {
         ERR("Line #%d too long", i);
-        return -2;
+        return 2;
     }
-    return i;
+    return 0;
 }
+
+int writeLines(FILE * ofile, lines_t * lines) {
+    for (int i = 0; i < lines->len; ++i) {
+        if (fputs((const char *)lines->vals[i], ofile) == EOF) {
+            ERR("Can\'t write line #%d", i);
+            return 1;
+        }
+        fputc('\n', ofile);
+    }
+    return 0;
+}
+
+int initLines(lines_t * lines) {
+    lines->vals = (letter **) calloc(MAX_LINES, sizeof(letter *));
+    if (lines->vals == NULL) {
+        ERR("Can't allocate space for lines");
+        return 1;
+    }
+    lines->len = 0;
+    return 0;
+}
+
+void sortLines(lines_t * lines) {
+    qsort(lines->vals, lines->len, sizeof(lines->vals[0]), cmpLines);
+}
+
+bool isLetter(letter c) {
+    return isalpha(c) || ((letter)'à' <= c && c <= (letter)'ÿ') || ((letter)'À' <= c && c <= (letter)'ß');
+}
+
+int cmpLines(const void * first, const void * second) {
+    letter * first_s = *(letter **)first;
+    letter * second_s = *(letter **)second;
+
+    int i = 0, j = 0;
+    while (i < MAX_LINE && j < MAX_LINE && first_s[i] && second_s[j]) {
+        if (isLetter(first_s[i]) && isLetter(second_s[j])) {
+            int res = (int)first_s[i] - (int)second_s[j];
+            if (res) return res;
+            i++; j++;
+            continue;
+        }
+        if (!isLetter(first_s [i])) i++;
+        if (!isLetter(second_s[j])) j++;
+    }
+    return 0;
+}
+
+void freeLines(lines_t * lines) {
+    for (int i = 0; i < lines->len; ++i) {
+        free(lines->vals[i]);
+    }
+    free(lines->vals);
+    lines->len = 0;
+}
+
