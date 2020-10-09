1d1a79abca8cd4af419bf2323a2ea828eca0a10c
abel1502 <terragame.abel@gmail.com>
2020-10-09 12:52:31 +0300

    logging.h Implement basic features

--- libs/logging.h
+++ libs/logging.h
@@ -11,11 +11,14 @@
 #include <stdio.h>
 //#include <assert.h>
 
+//================================================================================
+
 #ifndef MACROFUNC  // Same as in tests, but may not be included
 #define MACROFUNC(...) do {__VA_ARGS__} while (0)
 #endif
 
 #ifndef REQUIRE
+// Logger assertions mustn't be disableable, since an error in logging is possibly the most critical error possible
 #define REQUIRE(stmt)  MACROFUNC(                                                              \
     if (!(stmt)) {                                                                             \
         fprintf(stderr, "\nRequirement (%s) not met in (%s#%d)\n", #stmt, __FILE__, __LINE__); \
@@ -23,7 +26,7 @@
     } )
 #endif
 
-//--------------------------------------------------------------------------------
+//================================================================================
 
 const size_t LOGGER_MAX_BLOCKSTACK_DEPTH = 32;
 
@@ -33,7 +36,6 @@ typedef struct logger_s logger_t;
 typedef struct block_stack_s_ block_stack_t_;
 
 typedef const char *block_class_t;
-
 
 struct block_stack_s_ {
     size_t size;
@@ -45,6 +47,7 @@ struct logger_s {
     block_stack_t_ blockStack;
 };
 
+//--------------------------------------------------------------------------------
 
 logger_t *logger_open(logger_t *self, const char *fileName);
 
@@ -52,16 +55,19 @@ void logger_flush(logger_t *self);
 
 void logger_close(logger_t *self);
 
-void logger_blockStart(logger_t *self, block_class_t cls, ...);
+//void logger_blockStart(logger_t *self, block_class_t cls, ...);
 
-void logger_blockEnd(logger_t *self);
+//void logger_blockEnd(logger_t *self);
+
+void logger_write_(logger_t *self, const char *value);
 
 void logger_log(logger_t *self, const char *value);
 
 void logger_logf(logger_t *self, const char *format, ...);
 
+//--------------------------------------------------------------------------------
 
-static void block_stack_construct_(block_stack_t_ *self);
+block_stack_t_ *block_stack_construct_(block_stack_t_ *self);
 
 static void block_stack_push_(block_stack_t_ *self, block_class_t cls);
 
@@ -69,125 +75,157 @@ static block_class_t block_stack_pop_(block_stack_t_ *self);
 
 static void block_stack_free_(block_stack_t_ *self);
 
+//================================================================================
+
+#ifndef NO_LOG
+
+block_stack_t_ *block_stack_construct_(block_stack_t_ *self) {
+    REQUIRE(self != NULL);
+
+    self->size = 0;
+    self->buf = (block_class_t *)calloc(LOGGER_MAX_BLOCKSTACK_DEPTH, sizeof(block_class_t));
+
+    REQUIRE(self->buf != NULL);
+
+    return self;
+}
+
+static void block_stack_push_(block_stack_t_ *self, block_class_t cls) {
+    REQUIRE(self != NULL);
+    REQUIRE(self->buf != NULL);
+    REQUIRE(cls != NULL);
+
+    REQUIRE(self->size < LOGGER_MAX_BLOCKSTACK_DEPTH /* Sorry, LBS depth limit reached :( */ );
+
+    self->buf[self->size++] = cls;
+}
+
+static block_class_t block_stack_pop_(block_stack_t_ *self) {
+    REQUIRE(self != NULL);
+    REQUIRE(self->buf != NULL);
+
+    REQUIRE(self->size > 0 /* Sorry, LBS is empty :( */ );
+
+    return self->buf[--(self->size)];
+}
+
+static void block_stack_free_(block_stack_t_ *self) {
+    REQUIRE(self != NULL);
+    REQUIRE(self->buf != NULL);
+
+    REQUIRE(self->size == 0 /* Sorry, LBS is not empty :( */ );
+
+    free(self->buf);
+}
 
 //--------------------------------------------------------------------------------
 
+logger_t *logger_open(logger_t *self, const char *fileName) {
+    REQUIRE(self != NULL);
 
-#if 0
-static size_t blockDepth = 0;
-
-
-void log_tabulate() {
-    for (size_t i = 0; i < blockDepth; ++i) {
-        fprintf(stderr, "  ");
-    }
-}
-
-void log_token(const char *token) {
-    fprintf(stderr, "%s", token);
-}
-
-void log_space() {
-    log_token(" ");
-}
-
-void log_newline() {
-    log_token("\n");
-}
-
-void log_format(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-
-    vfprintf(stderr, format, args);
-
-    va_end(args);
-}
-
-void log_println(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-
-    log_tabulate();
-    vfprintf(stderr, format, args);
-    log_newline();
-
-    va_end(args);
-}
-
-void log_block_start(const char *name) {
-    log_tabulate();
-
-    if (name != NULL) {
-        log_token(name);
-        log_space();
-    }
-
-    log_token("{");
-
-    log_newline();
-
-    blockDepth++;
-}
-
-void log_block_end() {
-    assert(blockDepth > 0);
-    blockDepth--;
-
-    log_token("}");
-
-    log_newline();
-}
-
-void log_addr(const void *addr) {
-    log_token("[");
-
-    /*if (addr != NULL) {
-        log_token("0x");
-        log_format("%p", addr);
+    if (fileName == NULL) {
+        self->file = stderr;
     } else {
-        log_token("NULL");
-    }*/
+        self->file = fopen(fileName, "a");
+    }
 
-    log_token("0x");
-    log_format("%p", addr);
+    REQUIRE(self->file != NULL);
 
-    log_token("]");
+    block_stack_construct_(&self->blockStack);
+
+    //logger_blockStart(self, LBC_LOG);
+
+    return self;
 }
 
-//void log_var(const char *name, )
+void logger_flush(logger_t *self) {
+    REQUIRE(self != NULL);
+    REQUIRE(self->file != NULL);
 
+    fflush(self->file);
+}
 
-#define log_expr(expr, exprtype, format)                      \
-    do {                                                      \
-        log_println("%s = " format, #expr, (exprtype)(expr)); \
-    } while (0)
+void logger_close(logger_t *self) {
+    REQUIRE(self != NULL);
+    REQUIRE(self->file != NULL);
 
-#define log_expr_ptr(exprptr, exprtype, format)               \
-    do {                                                      \
-        exprtype *expr = (exprptr);                           \
-        log_tabulate();                                       \
-        log_addr(expr);                                       \
-        log_space();                                          \
-        if (expr == NULL) {                                   \
-            log_format("%s = <BAD!>\n", #exprptr);            \
-        } else {                                              \
-            log_format("%s = " format "\n", #exprptr, *expr); \
-        }                                                     \
-    } while (0)
+    //logger_blockEnd(self);
 
-#define log_expr_str(exprptr)                                 \
-    do {                                                      \
-        char *expr = (exprptr);                               \
-        log_tabulate();                                       \
-        log_addr(expr);                                       \
-        log_space();                                          \
-        if (expr == NULL) {                                   \
-            log_format("%s = <BAD!>\n", #exprptr);            \
-        } else {                                              \
-            log_format("%s = \"%s\"\n", #exprptr, expr);      \
-        }                                                     \
-    } while (0)
-#endif
+    if (self->file != stdout && self->file != stderr)
+        fclose(self->file);
+
+    self->file = NULL;
+
+    block_stack_free_(&self->blockStack);
+}
+
+//void logger_blockStart(logger_t *self, block_class_t cls, ...);
+
+//void logger_blockEnd(logger_t *self);
+
+void logger_write_(logger_t *self, const char *value) {
+    REQUIRE(self != NULL);
+    REQUIRE(self->file != NULL);
+    REQUIRE(value != NULL);
+
+    REQUIRE(fputs(value, self->file) != EOF);
+
+    logger_flush(self);
+}
+
+void logger_log(logger_t *self, const char *value) {
+    REQUIRE(self != NULL);
+    REQUIRE(self->file != NULL);
+    REQUIRE(value != NULL);
+
+    int res = 0;
+
+    while (*value != '\0') {
+        if (*value == '<') {
+            res |= fputs("&lt;", self->file);
+        } else if (*value == '>') {
+            res |= fputs("&gt;", self->file);
+        } else {
+            res |= fputc(*value, self->file);
+        }
+
+        ++value;
+    }
+
+    REQUIRE(res > 0 /* File output failed */ );
+
+    logger_flush(self);
+}
+
+void logger_logf(logger_t *self, const char *format, ...) {
+    REQUIRE(self != NULL);
+    REQUIRE(self->file != NULL);
+    REQUIRE(format != NULL);
+
+    va_list args = {};
+    va_start(args, format);
+
+    int requiredSize = vsnprintf(NULL, 0, format, args);
+
+    va_end(args);
+
+    char *buf = (char *)calloc(requiredSize + 1, sizeof(char));
+
+    va_start(args, format);
+
+    vsprintf(buf, format, args);
+
+    va_end(args);
+
+    logger_log(self, buf);
+
+    free(buf);
+}
+
+#else // NO_LOG
+
+// TODO
+
+#endif // NO_LOG
 
 #endif // LOGGINGLIB_GUARD
--- libs/logging_test.cpp
+++ libs/logging_test.cpp
@@ -3,7 +3,13 @@
 
 
 int main() {
-    //
+    logger_t logger = {};
+    logger_open(&logger, NULL);
+
+    logger_log(&logger, "Test\n");
+    logger_log(&logger, "Did you know that 1 < 2?\n");
+
+    logger_close(&logger);
 
     return 0;
 }
