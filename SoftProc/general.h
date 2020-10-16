685b738650d3f9c89196f87833bbaa7166ba2db1
abel1502 <terragame.abel@gmail.com>
2020-10-16 11:05:08 +0300

    SoftProc add general.h

---
+++ SoftProc/general.h
@@ -0,0 +1,10 @@
+#ifndef GENERAL_H_GUARD
+#define GENERAL_H_GUARD
+
+#ifdef NDEBUG
+#define ERR(msg, ...)
+#else
+#define ERR(msg, ...) do {fprintf(stderr, "[ERROR in %s() on #%d] " msg "\n", __func__, __LINE__, ##__VA_ARGS__); perror(">");} while (0)
+#endif
+
+#endif // GENERAL_H_GUARD
