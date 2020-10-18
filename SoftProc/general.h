266cdcbdb03e6c3dfab586c6bf41d2158971e9c4
abel1502 <terragame.abel@gmail.com>
2020-10-18 12:35:46 +0300

    SoftProc add c-compat bool

--- SoftProc/general.h
+++ SoftProc/general.h
@@ -7,4 +7,8 @@
 #define ERR(msg, ...) do {fprintf(stderr, "[ERROR in %s() on #%d] " msg "\n", __func__, __LINE__, ##__VA_ARGS__); /*perror(">");*/} while (0)
 #endif
 
+#ifndef __cplusplus
+typedef enum { false, true } bool;
+#endif
+
 #endif // GENERAL_H_GUARD
