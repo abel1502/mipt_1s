a8a0ad9545425530c63ff08383f8696dcc9445e7
abel1502 <terragame.abel@gmail.com>
2020-10-18 16:22:43 +0300

    SoftProc program.cpp implement program_read

--- SoftProc/program.cpp
+++ SoftProc/program.cpp
@@ -1,3 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 
 #include "program.h"
+
+
+const STACK_INITIAL_CAPACITY = 8;
+
+
+bool program_read(program_t *self, FILE *ifile) {
+    assert(self != NULL);
+    assert(ifile != NULL);
+
+    if (aef_mmap_read(&self->mmap, ifile)) {
+        ERR("Couldn't read the program file");
+        return true;
+    }
+
+    self->ip = 0;
+    self->registers = {};
+
+    if (stack_construct(&self->stack, STACK_INITIAL_CAPACITY) == NULL) {
+        ERR("Couldn't initialize program stack");
+        return true;
+    }
+
+    return false;
+}
+
+bool program_executeOpcode(program_t *self);
+
+bool program_execute(program_t *self);
+
+void program_free(program_t *self);
