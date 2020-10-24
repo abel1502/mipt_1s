3e98348669dbe7cdb95266a063a7efb2faf5fece
abel1502 <terragame.abel@gmail.com>
2020-10-24 19:47:41 +0300

    SoftProc new opcodes

--- SoftProc/opcode_defs.h
+++ SoftProc/opcode_defs.h
@@ -1,65 +1,189 @@
-DEF_OP(0x00, NOP , nop , 0, 0b0000000000000000, 0b00000000, {  })
+DEF_OP(0x00, NOP , nop , 0, 0b0000000000000000, 0b00000000, {})
 
-DEF_OP(0x01, PUSH, push, 1, 0b0100000000000000, 0b00000110, {  })
+DEF_OP(0x01, PUSH, push, 1, 0b0100000001010101, 0b00000110, {
+    PUSH_(ARG_);
+})
 
-DEF_OP(0x02, POP , pop , 1, 0b0100000000000000, 0b00000100, {  })
+DEF_OP(0x02, POP , pop , 1, 0b0100000001010101, 0b00000100, {
+    POP_(&self->registers[curOp.reg]);
+})
 
-DEF_OP(0x03, POPV, popv, 0, 0b0000000000000000, 0b00000000, {  })
+DEF_OP(0x03, POPV, popv, 0, 0b0000000000000000, 0b00000000, {
+    POP_(NULL);
+})
 
-DEF_OP(0x04, DUP , dup , 0, 0b0000000000000000, 0b00000000, {  })
+DEF_OP(0x04, DUP , dup , 0, 0b0000000000000000, 0b00000000, {
+    POP_(&tos0);
+    PUSH_(tos0);
+    PUSH_(tos0);
+})
 
-DEF_OP(0x05, ROT , rot , 1, 0b0000000000010000, 0b00000110, {  })
+DEF_OP(0x05, ROT , rot , 1, 0b0000000000010000, 0b00000110, {
+    switch (ARG_.dwl) {
+    case 0:
+    case 1:
+        break;
+    case 2:
+        POP_(&tos0);
+        POP_(&tos1);
+        PUSH_(tos0);
+        PUSH_(tos1);
+        break;
+    case 3:
+        POP_(&tos0);
+        POP_(&tos1);
+        POP_(&tos2);
+        PUSH_(tos0);
+        PUSH_(tos2);
+        PUSH_(tos1);
+        break;
+    default:
+        NOTIMPL_;
+    }
+})
 
-DEF_OP(0x08, ADD , add , 1, 0b0100000000000000, 0b00000001, {  })
+DEF_OP(0x08, ADD , add , 1, 0b0100000000000000, 0b00000001, { TMP_ONLYDOUBLE_
+    POP_(&tos0);
+	POP_(&tos1);
+	res.df = tos1.df + tos0.df;
+	PUSH_(res);
+})
 
-DEF_OP(0x09, SUB , sub , 1, 0b0100000000000000, 0b00000001, {  })
+DEF_OP(0x09, SUB , sub , 1, 0b0100000000000000, 0b00000001, { TMP_ONLYDOUBLE_
+    POP_(&tos0);
+	POP_(&tos1);
+	res.df = tos1.df - tos0.df;
+	PUSH_(res);
+})
 
-DEF_OP(0x0a, MUL , mul , 1, 0b0100000000000000, 0b00000001, {  })
+DEF_OP(0x0a, MUL , mul , 1, 0b0100000000000000, 0b00000001, { TMP_ONLYDOUBLE_
+    POP_(&tos0);
+	POP_(&tos1);
+	res.df = tos1.df * tos0.df;
+	PUSH_(res);
+})
 
-DEF_OP(0x0b, DIV , div , 1, 0b0100000000000000, 0b00000001, {  })
+DEF_OP(0x0b, DIV , div , 1, 0b0100000000000000, 0b00000001, { TMP_ONLYDOUBLE_
+    POP_(&tos0);
+	POP_(&tos1);
+	res.df = tos1.df / tos0.df;  // Zero division produces inf
+	PUSH_(res);
+})
 
-DEF_OP(0x0c, SQR , sqr , 1, 0b0100000000000000, 0b00000001, {  })
+DEF_OP(0x0c, SQR , sqr , 1, 0b0100000000000000, 0b00000001, { TMP_ONLYDOUBLE_
+    POP_(&tos0);
+	res.df = tos0.df * tos0.df;
+	PUSH_(res);
+})
 
-DEF_OP(0x0d, SQRT, sqrt, 1, 0b0100000000000000, 0b00000001, {  })
+DEF_OP(0x0d, SQRT, sqrt, 1, 0b0100000000000000, 0b00000001, { TMP_ONLYDOUBLE_
+    POP_(&tos0);
+	res.df = sqrt(tos0.df);
+	PUSH_(res);
+})
 
-DEF_OP(0x18, IN  , in  , 1, 0b0100000000000000, 0b00000101, {
+DEF_OP(0x18, IN  , in  , 1, 0b0100000000000000, 0b00000101, { TMP_ONLYDOUBLE_
     printf("(df) > ");
     if (AM_.locMem) {
         NOTIMPL_;
     } else if (AM_.locReg) {
-        scanf("%lg", &self->registers[curOp.reg].df);
+        if (scanf("%lg", &self->registers[curOp.reg].df) != 1) {
+            ERR("Cannot read input");
+            return true;
+        }
     } else {
-        scanf("%lg", &res.df);
+        if (scanf("%lg", &res.df) != 1) {
+            ERR("Cannot read input");
+            return true;
+        }
         PUSH_(res);
     }
 })
 
 DEF_OP(0x19, OUT , out , 1, 0b0100000000000000, 0b00000111, { TMP_ONLYDOUBLE_
-    printf("(df) > ");
+    printf("(df) ");
     if (AM_.loc) {
-        printf("%lg", ARG_);
+        printf("%lg", ARG_.df);
     } else {
         POP_(&tos0);
-        printf("%lg", tos0);
+        printf("%lg", tos0.df);
     }
     printf("\n");
 })
 
-DEF_OP(0x20, JMP , jmp , 1, 0b0000000000010000, 0b00000111, {  })
+DEF_OP(0x20, JMP , jmp , 1, 0b0000000000010000, 0b00000111, {
+    self->ip = ARG_.dwl;
+})
 
-DEF_OP(0x23, JE  , je  , 1, 0b0000000000010000, 0b00000111, {  })
+DEF_OP(0x21, JF  , jf  , 1, 0b0000000000010000, 0b00000111, {
+    POP_(&tos0);
+    if (!tos0.dwl) {
+        self->ip = ARG_.dwl;
+    }
+})
 
-DEF_OP(0x24, JNE , jne , 1, 0b0000000000010000, 0b00000111, {  })
+DEF_OP(0x22, JT  , jt  , 1, 0b0000000000010000, 0b00000111, {
+    POP_(&tos0);
+    if (tos0.dwl) {
+        self->ip = ARG_.dwl;
+    }
+})
 
-DEF_OP(0x25, JG  , jg  , 1, 0b0000000000010000, 0b00000111, {  })
+DEF_OP(0x23, CALL, call, 1, 0b0000000000010000, 0b00000111, {  })
 
-DEF_OP(0x26, JGE , jge , 1, 0b0000000000010000, 0b00000111, {  })
+DEF_OP(0x24, RET , ret , 0, 0b0000000000000000, 0b00000000, {  })
 
-DEF_OP(0x27, JL  , jl  , 1, 0b0000000000010000, 0b00000111, {  })
+#define EPSILON_ 1e-12
 
-DEF_OP(0x28, JLE , jle , 1, 0b0000000000010000, 0b00000111, {  })
+DEF_OP(0x28, CG  , cg  , 1, 0b0100000000000000, 0b00000001, { TMP_ONLYDOUBLE_
+    POP_(&tos0);
+	POP_(&tos1);
+	res.dwl = tos1.df > tos0.df + EPSILON_;
+	PUSH_(res);
+})
 
-DEF_OP(0xf0, END , end , 0, 0b0000000000000000, 0b00000000, { self->flags.flag_exit = true; })
+DEF_OP(0x29, CGE , cge , 1, 0b0100000000000000, 0b00000001, { TMP_ONLYDOUBLE_
+    POP_(&tos0);
+	POP_(&tos1);
+	res.dwl = tos1.df >= tos0.df - EPSILON_;
+	PUSH_(res);
+})
 
-DEF_OP(0xf1, DUMP, dump, 0, 0b0000000000000000, 0b00000000, { program_dump(self); })
+DEF_OP(0x2a, CL  , cl  , 1, 0b0100000000000000, 0b00000001, { TMP_ONLYDOUBLE_
+    POP_(&tos0);
+	POP_(&tos1);
+	res.dwl = tos1.df < tos0.df - EPSILON_;
+	PUSH_(res);
+})
+
+DEF_OP(0x2b, CLE , cle , 1, 0b0100000000000000, 0b00000001, { TMP_ONLYDOUBLE_
+    POP_(&tos0);
+	POP_(&tos1);
+	res.dwl = tos1.df <= tos0.df;
+	PUSH_(res);
+})
+
+DEF_OP(0x2c, CE  , ce  , 1, 0b0100000000000000, 0b00000001, { TMP_ONLYDOUBLE_
+    POP_(&tos0);
+	POP_(&tos1);
+	res.dwl = tos1.df <= tos0.df + EPSILON_ && tos1.df + EPSILON_ >= tos0.df;
+	PUSH_(res);
+})
+
+DEF_OP(0x2d, CNE , cne , 1, 0b0100000000000000, 0b00000001, { TMP_ONLYDOUBLE_
+    POP_(&tos0);
+	POP_(&tos1);
+	res.dwl = tos1.df > tos0.df + EPSILON_ || tos1.df + EPSILON_ < tos0.df;
+	PUSH_(res);
+})
+
+#undef EPSILON_
+
+DEF_OP(0xf0, END , end , 0, 0b0000000000000000, 0b00000000, {
+    self->flags.flag_exit = true;
+})
+
+DEF_OP(0xf1, DUMP, dump, 0, 0b0000000000000000, 0b00000000, {
+    program_dump(self);
+})
 
