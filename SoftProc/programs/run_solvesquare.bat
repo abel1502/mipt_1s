bd10ef4d04e670ba1036fddbe56734040b17f7aa
abel1502 <terragame.abel@gmail.com>
2020-10-25 13:17:12 +0300

    SoftProc reimplement SolveSquare in AEF

--- SoftProc/programs/run_solvesquare.bat
+++ SoftProc/programs/run_solvesquare.bat
@@ -1,2 +1,7 @@
-..\bin\Release\assembler.exe -i solvesquare.aaf -o solvequare.aef -vv
+@echo off
+
+..\bin\Release\assembler.exe -i solvesquare.aaf -o solvesquare.aef -vv
+
+IF %ERRORLEVEL% == 0 (
 ..\bin\Release\processor.exe -vv solvesquare.aef
+) ELSE ( ECHO [RSS.bat] failed to assemble program )
