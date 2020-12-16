2bdc05fafd74ed7b5e5d249d411716a83bd5d980
abel1502 <terragame.abel@gmail.com>
2020-12-16 13:56:32 +0300

    SoftLang begin parser

--- SoftLang/include/parser.h
+++ SoftLang/include/parser.h
@@ -1,4 +1,39 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include "general.h"
+#include "filebuf.h"
+#include "lexer.h"
+
+
+namespace SoftLang {
+
+    class Parser {
+    public:
+
+        enum Error_e {
+            ERR_PARSER_OK = 0,
+            ERR_PARSER_SYS,
+            ERR_PARSER_LEX,
+            ERR_PARSER_SYNTAX
+        };
+
+        FACTORIES(Parser)
+
+        bool ctor();
+
+        bool ctor(const FileBuf *src);
+
+        void dtor();
+
+        Error_e parse();
+
+    private:
+        Lexer lexer;
+
+    };
+
+}
+
+
 #endif // PARSER_H
--- SoftLang/source/parser.cpp
+++ SoftLang/source/parser.cpp
@@ -2,5 +2,37 @@
 
 
 namespace SoftLang {
-    //
+
+    bool Parser::ctor() {
+        TRY_B(lexer.ctor());
+
+        return false;
+    }
+
+    bool Parser::ctor(const FileBuf *src) {
+        TRY_B(lexer.ctor(src));
+
+        return false;
+    }
+
+    void Parser::dtor() {
+        lexer.dtor();
+    }
+
+    Parser::Error_e Parser::parse() {
+        if (lexer.parse()) {
+            return ERR_PARSER_SYS;
+        }
+
+        if (lexer.getError()) {
+            return ERR_PARSER_LEX;
+        }
+
+        if (!lexer.getEnd()) {
+            return ERR_PARSER_SYS;
+        }
+
+        // TODO
+    }
+
 }
