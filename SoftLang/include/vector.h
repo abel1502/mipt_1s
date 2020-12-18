1522316c2de841f7d8c4ffb1324cc1ed1d88edbb
abel1502 <terragame.abel@gmail.com>
2020-12-18 09:54:56 +0300

    SoftLang improve vector

--- SoftLang/include/vector.h
+++ SoftLang/include/vector.h
@@ -14,22 +14,25 @@ namespace SoftLang {
     class Vector {
     public:
 
-        static const unsigned DEFAULT_CAPACITY = 8;
+        static const unsigned DEFAULT_CAPACITY = 2;
 
-        FACTORIES(Vector);
+        FACTORIES(Vector<T>);
 
         bool ctor() {
-            buf = (T *)calloc(DEFAULT_CAPACITY, sizeof(T));
-            if (!buf)
-                return true;
+            buf = nullptr;
 
             size = 0;
-            capacity = DEFAULT_CAPACITY;
+            capacity = 0;
 
             return false;
         }
 
         void dtor() {
+            for (unsigned i = 0; i < size; ++i) {
+                buf[i].dtor();
+                buf[i] = nullptr;
+            }
+
             free(buf);
             buf = nullptr;
 
@@ -39,6 +42,7 @@ namespace SoftLang {
 
         T &operator[](size_t ind) {
             assert(ind < size);
+
             assert(buf);
 
             return buf[ind];
@@ -46,6 +50,7 @@ namespace SoftLang {
 
         const T &operator[](size_t ind) const {
             assert(ind < size);
+
             assert(buf);
 
             return buf[ind];
@@ -64,8 +69,9 @@ namespace SoftLang {
         }
 
         T &pop() {
-            assert(buf);
             assert(!isEmpty());
+
+            assert(buf);
 
             return buf[--size];
         }
@@ -90,6 +96,151 @@ namespace SoftLang {
         size_t capacity;
 
         bool resize(unsigned new_capacity) {
+            assert(new_capacity >= capacity);
+
+            if (new_capacity == 0)
+                new_capacity = DEFAULT_CAPACITY;
+
+            T *newBuf = (T *)realloc(buf, new_capacity * sizeof(T));
+            TRY_B(!newBuf);
+
+            buf = newBuf;
+            capacity = new_capacity;
+
+            return false;
+        }
+
+    };
+
+
+    template <typename T, unsigned COMPACT=1>
+    class CompactVector {
+    public:
+
+        static_assert(sizeof(T) <= sizeof(void *));
+
+        FACTORIES(CompactVector<T>);
+
+        bool ctor() {
+            buf = nullptr;
+
+            size = 0;
+            capacity = 0;
+
+            return false;
+        }
+
+        void dtor() {
+            if (isCompact()) {
+                for (unsigned i = 0; i < size; ++i) {
+                    compactBuf[i].dtor();
+                    compactBuf[i] = nullptr;
+                }
+            } else {
+                for (unsigned i = 0; i < size; ++i) {
+                    buf[i].dtor();
+                    buf[i] = nullptr;
+                }
+            }
+
+            free(buf);
+            buf = nullptr;
+
+            size = 0;
+            capacity = 0;
+        }
+
+        T &operator[](size_t ind) {
+            assert(ind < size);
+
+            if (isCompact()) {
+                return compactBuf[ind];
+            }
+
+            assert(buf);
+
+            return buf[ind];
+        }
+
+        const T &operator[](size_t ind) const {
+            assert(ind < size);
+
+            assert(buf);
+
+            return buf[ind];
+        }
+
+        bool append(const T &value) {
+            if (isCompact()) {
+                if (size + 1 <= COMPACT) {
+                    compactBuf[size++] = value;
+
+                    return false;
+                }
+
+                capacity = COMPACT + 1;
+                T *tmpBuf = (T *)calloc(capacity, sizeof(T));
+                TRY_B(tmpBuf);
+
+                for (unsigned i = 0; i < COMPACT; ++i) {
+                    tmpBuf[i] = compactBuf[i];
+                    compactBuf[i] = {};
+                }
+
+                buf = tmpBuf;
+            }
+
+            if (size >= capacity) {
+                TRY_B(resize(capacity * 2));
+
+                assert(capacity > size);
+            }
+
+            buf[size++] = value;
+
+            return false;
+        }
+
+        T &pop() {
+            assert(!isEmpty());
+
+            if (isCompact())
+                return compactBuf[--size];
+
+            assert(buf);
+
+            return buf[--size];
+        }
+
+        bool isEmpty() const {
+            return size == 0;
+        }
+
+        size_t getSize() const {
+            return size;
+        }
+
+        size_t getCapacity() const {
+            return isCompact() ? COMPACT : capacity;
+        }
+
+        constexpr bool isCompact() const {
+            return capacity == 0;
+        }
+
+    private:
+
+        union {
+            T *buf;
+
+            T compactBuf[COMPACT];
+        };
+
+        size_t size;
+        size_t capacity;
+
+        bool resize(unsigned new_capacity) {
+            assert(capacity != 0);
             assert(new_capacity >= capacity);
 
             T *newBuf = (T *)realloc(buf, new_capacity * sizeof(T));
