4e9a68e77c81d9c12c444ba3757404736e3c385d
abel1502 <terragame.abel@gmail.com>
2020-12-20 20:06:27 +0300

    SoftLang add read and meow pseudofuncs

--- SoftLang/dsl/pseudofuncs.dsl.h
+++ SoftLang/dsl/pseudofuncs.dsl.h
@@ -24,6 +24,24 @@ DEF_PFUNC(VOID, Void, print_int8, {
     PF_O("out qw:\n");
 })
 
+DEF_PFUNC(DBL, Dbl, read_dbl, {
+    PF_VERIFY_ARGCNT(0);
+
+    PF_O("in df:\n");
+})
+
+DEF_PFUNC(INT4, Int4, read_int4, {
+    PF_VERIFY_ARGCNT(0);
+
+    PF_O("in dwl:\n");
+})
+
+DEF_PFUNC(INT8, Int8, read_int8, {
+    PF_VERIFY_ARGCNT(0);
+
+    PF_O("in qw:\n");
+})
+
 DEF_PFUNC(DBL, Dbl, sqrt, {
     PF_VERIFY_ARGCNT(1);
 
@@ -33,7 +51,7 @@ DEF_PFUNC(DBL, Dbl, sqrt, {
     PF_O("sqrt df:\n");
 })
 
-DEF_PFUNC(DBL, Dbl, sqrt, {
+DEF_PFUNC(VOID, Void, meow, {
     PF_VERIFY_ARGCNT(1);
 
     PF_REQUIRE_CHILD_TYPE(0, Int4);
